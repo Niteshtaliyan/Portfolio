a6a2f8d93fcabcc8e5b59c4f425309badac7bc7a
7Assassins <abhinav.20m0141045@abes.ac.in>
2022-04-07 19:05:41 +0530

    Update urls.py

--- Nitesh/urls.py
+++ Nitesh/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
